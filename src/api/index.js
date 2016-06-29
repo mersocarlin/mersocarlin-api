98e2678a7d7a67087d6c769489e1aad431e2c333
mersocarlin <mersocarlin@gmail.com>
2016-06-29 20:46:33 +0200

    Add messages route

---
+++ src/api/index.js
@@ -0,0 +1,17 @@
+import * as message from './message';
+
+
+const wrap = fn => (...args) => fn(...args).catch(args[2]);
+
+
+export default (app) => {
+  app.post('/messages', wrap(message.create));
+
+  app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+    console.error(err.stack || err); // eslint-disable-line no-console
+
+    if (err) {
+      res.sendStatus(500);
+    }
+  });
+};
