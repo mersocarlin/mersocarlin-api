7ed2cf7b3cf362ea9da404d2fd18c0c4c0d609e5
mersocarlin <mersocarlin@gmail.com>
2016-07-04 19:56:01 +0200

    Validate request body before sending to message-service

--- src/services/message/create.js
+++ src/services/message/create.js
@@ -1,4 +1,5 @@
 import messageApiConsumer from '../../api-consumers/message';
+import validate from 'validate';
 
 
 export default async function create (services, data) {
@@ -6,9 +7,12 @@ export default async function create (services, data) {
 
   const entity = {
     name: data.name,
+    email: data.email,
     subject: data.subject,
     content: data.content,
   };
+
+  await validate(entity);
 
   return messageApi.createMessage(entity);
 }
