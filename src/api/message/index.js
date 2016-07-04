84f470b0cc3142bcbf5f0269d3d3c283900e8db7
mersocarlin <mersocarlin@gmail.com>
2016-07-04 11:55:05 +0200

    Send proper config according env variables

--- src/api/message/index.js
+++ src/api/message/index.js
@@ -1,3 +1,12 @@
+import { config } from '../../../config/env';
+import * as messageService from '../../services/message';
+
 export async function create (req, res) {
-  res.send(201, {});
+  res.send(
+    201,
+    await messageService.create(
+      config.services,
+      req.body
+    )
+  );
 }
