0c057ea7ad0c22dabcc23b4af36c5f6a2f4da9ed
mersocarlin <mersocarlin@gmail.com>
2016-07-14 22:40:27 +0200

    Remove deprecated method

--- src/api/message/index.js
+++ src/api/message/index.js
@@ -5,7 +5,6 @@ export async function create (req, res) {
   res
     .status(201)
     .send(
-      201,
       await messageService.create(
         req.app.get('config').services,
         req.app.get('config').keys,
